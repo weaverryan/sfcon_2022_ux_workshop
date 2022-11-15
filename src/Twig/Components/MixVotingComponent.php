ad3709549f34b13c280ac9ecd038350de38157d2
Ryan Weaver <ryan@thatsquality.com>
2022-11-15 15:56:40 +0100

    Step 49: LiveComponent: Simple example

--- src/Twig/Components/MixVotingComponent.php
+++ src/Twig/Components/MixVotingComponent.php
@@ -4,6 +4,7 @@ namespace App\Twig\Components;
 
 use App\Entity\VinylMix;
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\Attribute\LiveProp;
 use Symfony\UX\LiveComponent\DefaultActionTrait;
 
 #[AsLiveComponent('mix_voting')]
@@ -12,4 +13,7 @@ final class MixVotingComponent
     use DefaultActionTrait;
 
     public VinylMix $mix;
+
+    #[LiveProp(writable: true)]
+    public string $firstName = 'Ryan';
 }
