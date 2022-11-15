05a90b0634ddfd8b3c008c385e8a35ec31e68f3b
Ryan Weaver <ryan@thatsquality.com>
2022-11-15 15:56:39 +0100

    Step 46: LiveComponent: install it

--- config/bundles.php
+++ config/bundles.php
@@ -20,4 +20,6 @@ return [
     Symfony\UX\Autocomplete\AutocompleteBundle::class => ['all' => true],
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
     Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
+    Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
+    Symfony\UX\LiveComponent\LiveComponentBundle::class => ['all' => true],
 ];
