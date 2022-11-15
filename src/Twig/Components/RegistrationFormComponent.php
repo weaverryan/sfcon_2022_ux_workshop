e4a003e7dde0795246b0c6d424f582ffb67d3375
Ryan Weaver <ryan@thatsquality.com>
2022-11-15 15:56:43 +0100

    Step 60: LiveRegistration: use form trait

--- src/Twig/Components/RegistrationFormComponent.php
+++ src/Twig/Components/RegistrationFormComponent.php
@@ -2,11 +2,18 @@
 
 namespace App\Twig\Components;
 
+use Symfony\Component\Form\FormInterface;
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\ComponentWithFormTrait;
 use Symfony\UX\LiveComponent\DefaultActionTrait;
 
 #[AsLiveComponent('registration_form')]
 final class RegistrationFormComponent
 {
     use DefaultActionTrait;
+    use ComponentWithFormTrait;
+
+    protected function instantiateForm(): FormInterface
+    {
+    }
 }
