5d0666ef94819e854838a72bd089dad5d7e77cf9
Ryan Weaver <ryan@thatsquality.com>
2022-11-15 15:56:29 +0100

    Step 10: Stimulus: Add target & update count

--- assets/controllers/vote-controller.js
+++ assets/controllers/vote-controller.js
@@ -1,7 +1,11 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
+    static targets = ['total'];
+
     upVote() {
-        console.log('upvote!');
+        const total = parseInt(this.totalTarget.innerHTML.trim());
+
+        this.totalTarget.innerHTML = total + 1;
     }
 }
