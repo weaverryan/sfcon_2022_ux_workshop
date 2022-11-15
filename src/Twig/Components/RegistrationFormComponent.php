560237a5c76abd7242d095ebf6e3675b5980fb75
Ryan Weaver <ryan@thatsquality.com>
2022-11-15 15:56:43 +0100

    Step 61: LiveRegistration: fill in abstract method

--- src/Twig/Components/RegistrationFormComponent.php
+++ src/Twig/Components/RegistrationFormComponent.php
@@ -2,18 +2,21 @@
 
 namespace App\Twig\Components;
 
+use App\Form\RegistrationFormType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\FormInterface;
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
 use Symfony\UX\LiveComponent\ComponentWithFormTrait;
 use Symfony\UX\LiveComponent\DefaultActionTrait;
 
 #[AsLiveComponent('registration_form')]
-final class RegistrationFormComponent
+final class RegistrationFormComponent extends AbstractController
 {
     use DefaultActionTrait;
     use ComponentWithFormTrait;
 
     protected function instantiateForm(): FormInterface
     {
+        return $this->createForm(RegistrationFormType::class);
     }
 }
